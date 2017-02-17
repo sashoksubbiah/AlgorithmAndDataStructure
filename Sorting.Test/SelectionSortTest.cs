ae125b58e2496b179ff4b0eb8f72e68b1cbb189d
sashoksubbiah <ashoksubbiah@yahoo.in>
2017-02-17 15:25:29 +0530

    Selection Sort  Algorithm

---
+++ Sorting.Test/SelectionSortTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Sorting.Test
+{
+    [TestClass]
+    public class SelectionSortTest
+    {
+        [TestMethod]
+        public void SelectionSort_With_Valid_Input()
+        {
+            SelectionSort<int> selectionSort = new SelectionSort<int>();
+            int[] unsortedArray = { 3, 4, 8, 9, 6, 2, 1, 7, 5, 11, 13, 12, 19 };
+            var actual = selectionSort.Sort(unsortedArray).ToArray();
+            int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class SelectionSort<T> where T : IComparable<T>
+    {
+        //PesudoCode
+        //Ref : https://visualgo.net/sorting
+        //repeat(numOfElements - 1) times
+        //  set the first unsorted element as the minimum
+        //  for each of the unsorted elements
+        //      if element<currentMinimum
+        //       set element as new minimum
+        //  swap minimum with first unsorted position
+        public IEnumerable<T> Sort(IEnumerable<T> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException("input value cannot be null");
+            }
+            if (items.Count() == 0)
+            {
+                throw new ArgumentException("input collection should have atleast one value ");
+            }
+            ICollection<T> SortedArray = new List<T>();
+            ValueSwaper<T> swapper = new ValueSwaper<T>();
+            T unsortedArrayWithoutCurrentMin;
+            for (int i = 1; i < items.Count(); i++)
+            {
+              unsortedArrayWithoutCurrentMin =
+                                (SortedArray == null)
+                                ? items.Skip(i - 1).Min()
+                                : items.Except(SortedArray).Min();
+                SortedArray.Add(unsortedArrayWithoutCurrentMin);
+            }
+            var maxvalue = items.Max();
+            SortedArray.Add(maxvalue);
+            return SortedArray;
+        }
+    }
+}
